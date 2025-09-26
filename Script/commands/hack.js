49786ba50f64ed75e39497fe15433d14860b7ad1
alex-warren-007 <mdrazibhassan62@gmail.com>
2025-09-26 16:00:02 +0600

    Update hack.js

--- Script/commands/hack.js
+++ Script/commands/hack.js
@@ -6,7 +6,7 @@ module.exports = {
  config: {
  name: "hack",
  version: "1.0.3",
- hasPermssion: 2,
+ hasPermssion: 1,
  usePrefix: true,
  credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑀_ ☢️",
  description: "Prank friends with hack simulation",
